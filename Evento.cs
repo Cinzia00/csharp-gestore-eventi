3cc5174fe0b729e6661f6e33eb8fd7c0365b056c
CinziaPulvirenti <cinzia2092@hotmail.it>
2023-09-18 10:52:27 +0200

    creata classe evento

---
+++ Evento.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    internal class Evento
+    {
+
+        private string titolo;
+        private string data;
+        public  int CapienzaMassima { get; private set; }
+        public int PostiPrenotati { get; private set; }
+
+        public string Titolo
+        {
+            get
+            {
+                return titolo;
+            }
+            set
+            {
+                if(titolo == "")
+                {
+                    Console.WriteLine("Indicare il titolo dell'evento");
+                }
+            }
+        }
+
+        public string Data
+        {
+            get
+            {
+                return data;
+            }
+            set
+            {
+               DateTime dataOdierna = new DateTime();
+                dataOdierna =  DateTime.Now;
+
+                if (DateTime.CompareTo())
+                {
+
+                }
+
+            }
+        }
+
+
+
+        public Evento(string titolo, string data, int PostiPrenotati)
+        {
+            Titolo = titolo;
+            Data = data;
+            CapienzaMassima = 500;
+            PostiPrenotati = 0;
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-        }
+
+
+            Console.WriteLine();
+            Evento nuovoEvento = new Evento("asd", "20/09/2023", 21);
     }
 }
